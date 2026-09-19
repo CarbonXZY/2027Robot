c12fa81a03776022720ae2ac654caf703734c4a0
xzy <2411846114@qq.com>
2026-09-20 01:07:02 +0800

    略微调整了架构

--- build/chassis/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/carbon/Program_Projects/Control_Frame/src/chassis
-# Build directory: /home/carbon/Program_Projects/Control_Frame/build/chassis
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/chassis/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/carbon/Program_Projects/Control_Frame/install/shared_package/share/shared_package/package.sh"
